1785ea758bc7c6e20409125c64ad424b8deacb80
Jagalsan <jagalerasanchez@gmail.com>
2024-06-01 15:53:41 +0200

    feat: add minimize option when start dev mode

--- next.config.js
+++ next.config.js
@@ -4,4 +4,15 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.mode = 'development';
+      config.optimization.minimize = false;
+    } else {
+      config.mode = 'production';
+      config.optimization.minimize = true;
+    }
+
+    return config;
+  }
 }
