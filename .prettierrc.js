484c0717c929828eeb13dbbc9f0b9c9cdd749722
Jagalsan <jagalerasanchez@gmail.com>
2024-06-01 15:52:48 +0200

    chore: create project

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    singleQuote: true,
+    tabWidth: 4,
+    overrides: [
+        {
+            files: '*.yml',
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
